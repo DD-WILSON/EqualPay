90dbba006e22726de29cd7acc1452b7bd77322e9
Dan W <Dan@wilwat.com>
2025-06-25 20:56:33 +0100

    Package Imports set up

--- Oxaca_Model.py
+++ Oxaca_Model.py
@@ -1,1 +1,8 @@
-##
+## Importing Packages
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+import statsmodels.api as sm
+import pandas as pd
